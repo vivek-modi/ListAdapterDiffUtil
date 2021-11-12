a4bfc58fbc3f3973a3a9402579158c637beee40c
Vivek Modi <vivekmodi456@gmail.com>
2021-11-12 14:13:08 +0000

    added diff uitl

--- app/src/main/java/com/example/listadapter/Adapter.kt
+++ app/src/main/java/com/example/listadapter/Adapter.kt
@@ -12,13 +12,16 @@ class Adapter(
     companion object {
         private val LIST_COMPARATOR = object : DiffUtil.ItemCallback<XYZ>() {
             override fun areItemsTheSame(oldItem: XYZ, newItem: XYZ): Boolean {
-                return oldItem.id == newItem.id
+                return ((oldItem.id == newItem.id) && (oldItem.item?.id == newItem.item?.id)
+                        && (oldItem.item?.rr?.id == newItem.item?.rr?.id))
             }
 
             override fun areContentsTheSame(oldItem: XYZ, newItem: XYZ): Boolean {
                 return ((oldItem.title == newItem.title) && (oldItem.status == newItem.status)
                         && (oldItem.count == newItem.count)
-                        && (oldItem.item == newItem.item))
+                        && (oldItem.item == newItem.item)
+                        && (oldItem.item?.rr?.firstName == newItem.item?.rr?.firstName)
+                        && (oldItem.item?.rr?.lastName == newItem.item?.rr?.lastName))
             }
         }
     }
